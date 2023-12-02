91d1a34f29fba49762ec797b618a0c604a7fcebb
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-02 16:30:34 -0600

    created welcome message for user

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useAuth } from '@/composables/useAuth'
 
-const { isAuthenticated, logout } = useAuth()
+const { isAuthenticated, logout, user } = useAuth()
 
   const brand = ref('🏢 Fake Company Directory')
 </script>
@@ -14,6 +14,7 @@ const { isAuthenticated, logout } = useAuth()
         <span class="brand-title">{{ brand }}</span>
       </RouterLink>
       <div class="menu">
+        <p v-show="isAuthenticated" class="px-2 py-4">Welcome back, <strong><i>{{ user.name }}</i></strong></p>
         <div v-if="isAuthenticated">
         <RouterLink :to="{ name: 'Home' }" href="#" class="menu-item">Settings</RouterLink>
         <button href="#" class="menu-logout" @click="logout">Logout</button>
