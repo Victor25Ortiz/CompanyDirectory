b9607a861c68f609ac2891642a9225bd605344e2
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-02 15:32:34 -0600

    create login page

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,3 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import { useAuth } from '@/composables/useAuth'
+const { login } = useAuth()
+
+const username = ref('')
+const password = ref('')
+
+const logUserIn = () => {
+    if (login(username.value, password.value)) {
+        console.log('loggin in')
+    }
+}
+</script>
+
 <template>
-    <h1>Login Page</h1>
+    <form class="login-form" @submit.prevent="logUserIn">
+    <input v-model="username" type="text" placeholder="Username" />
+    <input v-model="password" type="text" placeholder="Password" />
+    <button type="submit" class="bg-green-500 px-4 py-2" @click="login('admin', 'admin')">Log In</button>
+    </form>
 </template>
+
+<style scoped lang="postcss">
+.login-form {
+    @apply mx-auto mt-80 flex max-w-md flex-col gap-4 rounded-md bg-white p-8 shadow-lg;
+    & input {
+        @apply text-xl px-4 py-2 ring-1 rounded-md ring-slate-300;
+    }
+}
+</style>
